7801f1d6cd15c41d1a9870d36d9c026945298f60
nandofalou <nando.falou@gmail.com>
2021-10-27 23:50:09 -0300

    fix: home view

--- app/Views/welcome_message.php
+++ app/Views/welcome_message.php
@@ -15,7 +15,7 @@
 
                 <h1>Welcome to CodeIgniter <?= CodeIgniter\CodeIgniter::CI_VERSION ?></h1>
 
-                <h2>The small framework with powerful features</h2>
+                
 
             </div>
 
@@ -25,17 +25,7 @@
 
         <section>
 
-            <h1>About this page</h1>
-
-            <p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
-
-            <p>If you would like to edit this page you will find it located at:</p>
-
-            <pre><code>app/Views/welcome_message.php</code></pre>
-
-            <p>The corresponding controller for this page can be found at:</p>
-
-            <pre><code>app/Controllers/Home.php</code></pre>
+            <h1>Banco de dados atualizados</h1>
 
         </section>
 
